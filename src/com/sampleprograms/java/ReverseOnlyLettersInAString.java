c7f9ba3f6ef495583504655d53db1886afa0ea6c
biabraham <bibinabraham2010@gmail.com>
2015-02-04 13:40:55 -0800

    modified ReverseOnlyLettersInAString.java class to accommodate special character handling

--- src/com/sampleprograms/java/ReverseOnlyLettersInAString.java
+++ src/com/sampleprograms/java/ReverseOnlyLettersInAString.java
@@ -6,20 +6,25 @@ import java.util.List;
 public class ReverseOnlyLettersInAString {
 	
 	public static void reverseOnlyLetters(String input){
-		
+		char ch;
 		char[] charArray = input.toCharArray();
 		List<Character> list = new ArrayList<Character>();
 		for(int i=0;i<charArray.length;i++){
-			if(Character.isDigit(charArray[i])){
+			ch=charArray[i];
+			if(((int) ch >=65 && (int) ch<=90)||((int) ch>=97 && (int) ch <=122)){
+				list.add(charArray[i]);
+				charArray[i]=' ';
+			}
+			/*if(Character.isDigit(charArray[i])){
 				continue;
 			}
 			list.add(charArray[i]);
-			charArray[i]='~';
+			charArray[i]='~';*/
 		}
 		
 		int k = list.size()-1;
 		for(int i=0;i<charArray.length;i++){
-			if(charArray[i]=='~'){
+			if(charArray[i]==' '){
 				charArray[i] = list.get(k);
 				k--;
 			}
@@ -30,7 +35,7 @@ public class ReverseOnlyLettersInAString {
 
 	public static void main(String[] args) {
 		
-		String input = "ab23cd";
+		String input = "ab23$b4cd";
 		reverseOnlyLetters(input);
 		// TODO Auto-generated method stub
 
